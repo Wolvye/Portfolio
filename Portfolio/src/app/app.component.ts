561d54b3601863402157e9afaf3593b6c7c0f011
Wolvye <alainsoltau@gmail.com>
2024-07-28 12:02:43 +0200

    Update

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MainComponent,FooterComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
