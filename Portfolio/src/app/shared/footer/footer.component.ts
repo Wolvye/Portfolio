3c8a9c61e9e19477ebe6b9c7d0548830e0e8c4a4
Wolvye <alainsoltau@gmail.com>
2024-08-13 16:03:36 +0200

    update

--- Portfolio/src/app/shared/footer/footer.component.ts
+++ Portfolio/src/app/shared/footer/footer.component.ts
@@ -1,18 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { TranslationService } from '../../translation.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
 
 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
   translate = inject(TranslationService);
+  imprint: string | any[] | null | undefined;
+
 
 
 }
