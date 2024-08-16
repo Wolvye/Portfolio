d43732c2697fb36ebe71ff38edcbe07a32bbac21
Wolvye <alainsoltau@gmail.com>
2024-08-16 18:19:16 +0200

    Update

--- Portfolio/src/app/contact/contact/contact.component.ts
+++ Portfolio/src/app/contact/contact/contact.component.ts
@@ -4,15 +4,20 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { TranslationService } from '../../translation.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule,TranslateModule,ReactiveFormsModule,],
+  imports: [FormsModule, TranslateModule, ReactiveFormsModule, RouterModule, RouterLink,],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  router = inject(Router);
   http = inject(HttpClient);
   mailTest = false;
   isChecked = false;
@@ -36,13 +41,13 @@ export class ContactComponent {
       },
     },
   };
-isPopupVisible: any;
-popupMessage: any;
+  isPopupVisible: any;
+  popupMessage: any;
 
   onSubmit(ngForm: NgForm) {
-    if (!this.isChecked && ngForm.submitted && ngForm.form.valid && !this.mailTest ) {
-      
-    }else{
+    if (!this.isChecked && ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+
+    } else {
     }
     if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
       this.http.post(this.post.endPoint, this.post.body(this.contactData))
@@ -63,4 +68,20 @@ popupMessage: any;
     }
   }
 
+  
+  scrollToTop(): void {
+    this.router.navigate(['/imprint']).then(() => {
+      window.scrollTo(0, 0);
+    });
+  }
+
+  constructor() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 }
+
+
