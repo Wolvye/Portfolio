8958e14aab19dc2a07d09af867d672f5221654a3
Wolvye <alainsoltau@gmail.com>
2024-08-16 09:22:26 +0200

    Update

--- Portfolio/src/app/shared/footer/footer.component.ts
+++ Portfolio/src/app/shared/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { TranslationService } from '../../translation.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 
 
@@ -17,7 +18,14 @@ export class FooterComponent {
 
   translate = inject(TranslationService);
   imprint: string | any[] | null | undefined;
+  router=inject(Router);
 
-
-
+  scrollToTop(): void {
+    this.router.navigate(['/imprint']);
+      window.scrollTo({
+         top: 0,
+         left: 0,
+         behavior: 'smooth' 
+        });
+      }
 }
